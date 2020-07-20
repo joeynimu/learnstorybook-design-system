06e7039774587536794eb171eed24be27dd244a3
Joe Ng'ethe <joeynimu@gmail.com>
2020-07-20 12:16:00 +0300

    added more config docs and an intro story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  // automatically import all files ending in *.stories.js|mdx
+  stories: ['../src/Intro.stories.mdx','../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
@@ -7,11 +8,6 @@ module.exports = {
     '@storybook/addon-storysource',
     '@storybook/addon-knobs',
     '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-      },
-    },
+    '@storybook/addon-docs',
   ],
 };
